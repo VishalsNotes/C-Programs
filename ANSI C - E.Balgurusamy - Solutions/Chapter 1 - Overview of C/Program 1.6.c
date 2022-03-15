923e82cf329906abfc78dcfca3f98bc2b653fc81
VishalShenoy2002 <vishal.bshenoy@gmail.com>
2022-03-15 07:04:46 +0530

    Added two files

---
+++ ANSI C - E.Balgurusamy - Solutions/Chapter 1 - Overview of C/Program 1.6.c
@@ -0,0 +1,13 @@
+// Write a Program to print multiplication table of 5.
+
+#include<stdio.h>
+
+void main()
+{
+    int i;
+
+    for(i=1;i<=10;i++)
+    {
+        printf("5 X %2d = %2d\n",i,i*5);
+    }
+}
